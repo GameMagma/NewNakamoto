129012b0bc9f9c0af9a9bff6ad56ec709bf41a01
Connor Midgley <53975931+GameMagma@users.noreply.github.com>
2023-10-18 13:32:23 -0500

    Refactor initiative into subcommand groups
    Put all of the initiative commands into one subcommand group.

--- main.py
+++ main.py
@@ -2,10 +2,103 @@ import os
 from dotenv import load_dotenv
 
 import interactions
-from interactions import slash_command, SlashContext, OptionType, slash_option
+from interactions import slash_command, SlashContext, OptionType, slash_option, listen
 
 load_dotenv()
 bot = interactions.Client(intents=interactions.Intents.ALL)
+
+
+# === EVENTS ===
+
+
+@listen()
+async def on_startup():
+    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
+    print("------\n")
+
+
+# === COMMANDS ===
+
+
+@slash_command(
+    name="ping",
+    description="Ping the bot to see if it's alive."
+)
+async def ping(ctx: SlashContext):
+    await ctx.send("Pong!")
+
+
+# === INITIATIVE COMMANDS ===
+
+
+@slash_command(
+    name="initiative",
+    description="Commands for the initiative tracker.",
+    sub_cmd_name="clear",
+    sub_cmd_description="Clear the initiative list"
+)
+async def initiative_clear(ctx: SlashContext):
+    globals().get("roll_list").clear()
+    await ctx.send("Roll list cleared.")
+
+
+@slash_command(
+    name="initiative",
+    description="Commands for the initiative tracker.",
+    sub_cmd_name="roll",
+    sub_cmd_description="Submit a roll for the current encounter"
+)
+@slash_option(
+    name="roll_result",
+    description="The number you rolled.",
+    required=True,
+    opt_type=OptionType.INTEGER
+)
+async def initiative_roll(ctx: SlashContext, roll_result: int):
+    # Add roll to the roll list
+    set_roll(ctx.author.id, roll_result)
+    await ctx.send(f"Roll of {roll_result} added.")
+
+
+@slash_command(
+    name="initiative",
+    description="Commands for the initiative tracker.",
+    sub_cmd_name="npc_roll",
+    sub_cmd_description="Submit an NPC's roll for the current encounter."
+)
+@slash_option(
+    name="roll_result",
+    description="The number you rolled.",
+    required=True,
+    opt_type=OptionType.INTEGER
+)
+@slash_option(
+    name="npc_name",
+    description="The name of the NPC.",
+    required=True,
+    opt_type=OptionType.STRING
+)
+async def initiative_npc_roll(ctx: SlashContext, roll_result: int, npc_name: str):
+    # Add roll to the roll list
+    set_roll(npc_name, roll_result)
+    await ctx.send(f"Roll of {roll_result} added for {npc_name}.")
+
+
+@slash_command(
+    name="initiative",
+    description="Commands for the initiative tracker.",
+    sub_cmd_name="get_order",
+    sub_cmd_description="Displays the roll order for the current encounter."
+)
+async def initiative_get_order(ctx: SlashContext):
+    rolls = get_roll_list()
+    if len(rolls) == 0:
+        await ctx.send("No rolls have been submitted yet.")
+    else:
+        msg = "Initiative order:\n"
+        for i, (key, value) in enumerate(rolls):
+            msg += f"{i + 1}. {key} ({value})\n"
+        await ctx.send(msg)
 
 roll_list = {}  # List of rolls for the current encounter
 
@@ -35,77 +128,6 @@ def set_roll(characterID: int | str, die_result: int) -> None:
     :param die_result: Number that was rolled
     """
     roll_list.update({characterID: die_result})
-
-
-@slash_command(
-    name="ping",
-    description="Ping the bot to see if it's alive."
-)
-async def ping(ctx: SlashContext):
-    await ctx.send("Pong!")
-
-
-# TODO: Make these a group command
-@slash_command(
-    name="start_encounter",
-    description="Indicate the start of an encounter. This will scrap any old rolls and get ready for new rolls."
-)
-async def start_encounter(ctx: SlashContext):
-    globals().get("roll_list").clear()
-    await ctx.send("New encounter started")
-
-
-@slash_command(
-    name="roll",
-    description="Submit a roll for the current encounter."
-)
-@slash_option(
-    name="roll_result",
-    description="The number you rolled.",
-    required=True,
-    opt_type=OptionType.INTEGER
-)
-async def roll(ctx: SlashContext, roll_result: int):
-    # Add roll to the roll list
-    set_roll(ctx.author.id, roll_result)
-    await ctx.send(f"Roll of {roll_result} added.")
-
-
-@slash_command(
-    name="npc_roll",
-    description="Submit an NPC's roll for the current encounter."
-)
-@slash_option(
-    name="roll_result",
-    description="The number you rolled.",
-    required=True,
-    opt_type=OptionType.INTEGER
-)
-@slash_option(
-    name="npc_name",
-    description="The name of the NPC.",
-    required=True,
-    opt_type=OptionType.STRING
-)
-async def npc_roll(ctx: SlashContext, roll_result: int, npc_name: str):
-    # Add roll to the roll list
-    set_roll(npc_name, roll_result)
-    await ctx.send(f"Roll of {roll_result} added for {npc_name}.")
-
-
-@slash_command(
-    name="get_init_order",
-    description="Displays the roll order for the current encounter."
-)
-async def get_init_order(ctx: SlashContext):
-    rolls = get_roll_list()
-    if len(rolls) == 0:
-        await ctx.send("No rolls have been submitted yet.")
-    else:
-        msg = "Initiative order:\n"
-        for i, (key, value) in enumerate(rolls):
-            msg += f"{i + 1}. {key} ({value})\n"
-        await ctx.send(msg)
 
 
 bot.start(os.getenv("DISCORD_TOKEN"))
